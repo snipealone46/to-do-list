ad29a25f75c618f69f994b61078c3a4e93d0e095
Eric Yang <eric.shaohuiyang@gmail.com>
2016-07-21 20:49:29 -0700

    Refactor feature test

--- spec/features/to_do_lists/create_spec.rb
+++ spec/features/to_do_lists/create_spec.rb
@@ -1,32 +1,31 @@
 require 'rails_helper'
 
 describe "Creating todo lists" do
+  def create_todo_list(options={})
+    options[:title] ||= "My todo list"
+    options[:description] ||= "This is the default description"
+    visit '/'
+    click_on 'New To do list'
+    expect(page).to have_content('New To Do List')
+
+    fill_in 'Title', with: options[:title]
+    fill_in 'Description', with: options[:description]
+    click_button 'Create To do list'
+  end
+
   it "redirects to the todo list index page on success" do
-     visit '/'
-     click_on 'New To do list'
-     expect(page).to have_content('New To Do List')
-
-     fill_in 'Title', with: 'test'
-     fill_in 'Description', with: 'test'
-     click_button 'Create To do list'
-
-     expect(page).to have_content('test')
+    create_todo_list title: 'test'
+    expect(page).to have_content('test')
   end
 
   it "display an error when the to do list has less than 3 characters" do
-     expect(ToDoList.count).to eq(0)
-     visit '/'
-     click_on 'New To do list'
-     expect(page).to have_content('New To Do List')
+    expect(ToDoList.count).to eq(0)
+    create_todo_list  title: 'hi'
 
-     fill_in 'Title', with: 'hi'
-     fill_in 'Description', with: 'hi'
-     click_button 'Create To do list'
+    expect(ToDoList.count).to eq(0)
+    expect(page).to have_content("error")
 
-     expect(ToDoList.count).to eq(0)
-     expect(page).to have_content("error")
-
-     visit "/"
-     expect(page).to_not have_content("hi")
+    visit "/"
+    expect(page).to_not have_content("hi")
   end
 end
